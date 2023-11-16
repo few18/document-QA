f591f4baffb7565626b2461196fc72181dbd73eb
Filip Weidler <filip.weidler@gmail.com>
2023-11-16 10:31:03 +0100

    added functionality to ask gpt

--- app/home.py
+++ app/home.py
@@ -2,14 +2,17 @@ import numpy as np
 import openai
 import pandas as pd
 import streamlit as st
-from utils import load_data
-import os
+from utils import load_data, ask_gpt
 
-
+st.set_page_config(
+    page_title="Chat with Your Document",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
 st.header("Chat with Your Document")
 
 client = openai.OpenAI()
-
 
 # Initialize chat message history
 if "messages" not in st.session_state.keys():
@@ -18,7 +21,7 @@ if "messages" not in st.session_state.keys():
     ]
 
 # Either load embeddings from storage or create via openai api
-embeddings = load_data()
+faq_embeddings = load_data()
 
 # Prompt for user input and save to chat history
 if query := st.chat_input("Your question"):
@@ -30,12 +33,18 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 
+ending = """
+
+May I help you with any other questions you have regarding your document?
+"""
+
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
+            response = ask_gpt(query, faq_embeddings, client, n=2)
+            output = response + ending
+            st.write(output)
+            message = {"role": "assistant", "content": output}
             # Add response to message history
-            st.session_state.messages.append(message)
+            st.session_state.messages.append(output)
